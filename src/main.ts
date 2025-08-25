3e99e2f8c0a0d421ec7ac309106fb1eee952a858
jaehongjun <gatsby0391@naver.com>
2025-08-25 14:18:46 +0900

    cors 허용

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // CORS 설정
+  // app.enableCors({
+  //   origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  //   credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  // });
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
